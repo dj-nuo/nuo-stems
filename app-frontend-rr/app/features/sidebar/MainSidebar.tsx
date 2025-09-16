d166a9f49a2a6c32c67d87c251c51604b7d83412
DJ NUO <djnuo95@gmail.com>
2025-09-16 12:54:02 +0200

    Refactor sidebar component by extracting menu items into a separate file
    - Moved menu items from MainSidebar.tsx to a new menuItems.ts file for better organization and maintainability.
    - Updated MainSidebar to import menu items from the new file.

--- app-frontend-rr/app/features/sidebar/MainSidebar.tsx
+++ app-frontend-rr/app/features/sidebar/MainSidebar.tsx
@@ -1,37 +1,8 @@
 import { useSidebar } from "~/components/ui/sidebar";
 
-import { Home, Settings, Rows3, ToolCase, Disc3 } from "lucide-react";
 import { useAppVersion } from "~/lib/rpc/fetchers";
 import MainSidebarView from "./MainSidebarView";
-
-// Menu items.
-const menuItems = [
-  {
-    title: "Home",
-    url: "/",
-    icon: Home,
-  },
-  {
-    title: "Convert Queue",
-    url: "/queue",
-    icon: Rows3,
-  },
-  {
-    title: "Traktor Pro 4",
-    url: "/traktor-pro-4",
-    icon: Disc3,
-  },
-  {
-    title: "Traktor Utilities",
-    url: "/traktor-utilities",
-    icon: ToolCase,
-  },
-  {
-    title: "Settings",
-    url: "/settings",
-    icon: Settings,
-  },
-];
+import { menuItems } from "./menuItems";
 
 // Container (keeps data close to use-site)
 export default function MainSidebar() {
